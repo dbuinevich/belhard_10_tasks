0a343f86de184c5eed6ba17adfb21441600fc4c9
Darya Buinevich <buinevich.darya@gmail.com>
2021-09-03 01:41:20 +0300

    * both tasks

--- tasks/file_tasks/reverse_rows.py
+++ tasks/file_tasks/reverse_rows.py
@@ -3,3 +3,19 @@
 файл. Создать новый файл, каждая строка которого получается из соответствующей
 строки исходного файла перестановкой слов в обратном порядке.
 """
+
+
+def revert_rows():
+    text = open('text.txt', 'r+')
+    reverted_text = open('reverted_text.txt', 'a')
+    lines_to_revert = text.readlines()
+    for line in lines_to_revert:
+        change_line = line.split(' ')
+        change_line[-1] = change_line[-1].removesuffix('.\n')
+        change_line.reverse()
+        new_line = ' '.join(change_line)
+        reverted_text.write(new_line + '\n')
+
+
+if __name__ == '__main__':
+    revert_rows()
